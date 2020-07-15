85f1d7854a2cb903ee7b67c3d20412303c301f49
no3g <thhoang296@gmail.com>
2020-07-15 15:54:07 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,5 @@
- <?php 
- header("Location: http://google.com"); 
- ?>
+<?php
+// PHP permanent URL redirection test
+header("Location: https://www.rapidtables.com/web/dev/php-redirect.html", true, 301);
+exit();
+?>
